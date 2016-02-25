1f286d2565ba0f78d8088edf17fd490606dfa900
Wei-Lun CHANG <maomaoto@gmail.com>
2016-02-25 23:57:29 +0800

    Add 1_2_ascend_order.cpp

---
+++ Assignment_1_1_bubble_sort/1_2_ascend_order.cpp
@@ -0,0 +1,17 @@
+// assignment 1-2
+// 奇偶排序
+// 輸入十個整數，按照升序排列輸出，並且奇數在前，偶數在後
+#include <iostream>
+using namespace std;
+
+int main() {
+    // read 10 numbers
+    int a[10];
+    for (int i = 0; i<10; i++) {
+        cin >> a[i];
+    }
+
+    // First, arrange odd numbers form left hand side, arrange even numbers from right hand side
+    int l = 0;
+    int r = 9;
+}
